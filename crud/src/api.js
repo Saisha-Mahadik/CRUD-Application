a70701361756a8a88c03b79df36ced5ca10cc144
Saisha-Mahadik <136926547+Saisha-Mahadik@users.noreply.github.com>
2024-11-21 23:05:53 +0530

    Update api.js

--- crud/src/api.js
+++ crud/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://localhost:5000/api/posts' 
+    baseURL: 'https://crud-application-backend-dvd1.onrender.com/api/posts' 
 });
 
 export const fetchPosts = () => API.get('/');
