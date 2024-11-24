1f42d53dc46aa1ccb091db9d674a2a276907f564
Saisha-Mahadik <136926547+Saisha-Mahadik@users.noreply.github.com>
2024-11-25 00:23:03 +0530

    Update dbConfig.js

--- server/src/configration/dbConfig.js
+++ server/src/configration/dbConfig.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 const connectedToMongoDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/crud_db");
+    await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
 
     console.log("Connected to MongoDB");
   } catch (error) {
