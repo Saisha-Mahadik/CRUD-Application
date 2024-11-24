064ca7a0f41536da127a34acff1ba3bf7147efb1
Saisha-Mahadik <136926547+Saisha-Mahadik@users.noreply.github.com>
2024-11-25 00:33:13 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const { connectedToMongoDB } = require('./src/configration/dbConfig');
 const postRoutes = require('./src/routes/postRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -14,8 +14,8 @@ app.use('/api/posts', postRoutes);
 
 connectedToMongoDB();
 
-app.listen(PORT, () => {
-    console.log(`Server is running on :${PORT}`);
+app.listen(port, () => {
+    console.log(`Server is running on :${port}`);
 });
 
 
